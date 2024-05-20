91149d0efbc7fdfccdeec06f570c7d1b96b0499a
Majorfi <major@ycorpo.com>
2024-05-20 10:43:37 +0200

    chore: bump

--- src/utils/wagmi/utils.ts
+++ src/utils/wagmi/utils.ts
@@ -171,13 +171,13 @@ function initIndexedWagmiChains(): TNDict<TExtendedChain> {
 				wrappedToken: wrappedChainTokens[extendedChain.id]
 			};
 
+			const newJsonRPCURL = process.env[`RPC_URI_FOR_${extendedChain.id}`];
 			const oldJsonRPCURL = process.env.JSON_RPC_URL?.[extendedChain.id];
-			if (oldJsonRPCURL) {
+			if (!newJsonRPCURL && oldJsonRPCURL) {
 				console.debug(
 					`JSON_RPC_URL[${extendedChain.id}] is deprecated. Please use RPC_URI_FOR_${extendedChain.id}`
 				);
 			}
-			const newJsonRPCURL = process.env[`RPC_URI_FOR_${extendedChain.id}`];
 			const defaultJsonRPCURL = extendedChain?.rpcUrls?.public?.http?.[0];
 
 			extendedChain.defaultRPC = newJsonRPCURL || oldJsonRPCURL || defaultJsonRPCURL || '';
